531ed9211028367a957f4ec0d817b38cf4f4442b
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-05-15 01:08:16 +0200

    Update custom mage target

--- Magefile.go
+++ Magefile.go
@@ -8,7 +8,7 @@ import (
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
-// Hello prints a message. Here to show that you can define custom Mage targets.
+// Hello prints a message (shows that you can define custom Mage targets).
 func Hello() {
 	fmt.Println("hello plugin developer!")
 }
