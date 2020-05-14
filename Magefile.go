417a9a8af47551d8fae1fea76a2ebc35eae22673
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2020-05-14 17:57:21 +0200

    Adds a custom target to show that custom targets can be defined

--- Magefile.go
+++ Magefile.go
@@ -3,9 +3,15 @@
 package main
 
 import (
+	"fmt"
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
+
+// Hello prints a message. Here to show that you can define custom Mage targets.
+func Hello() {
+	fmt.Println("hello plugin developer!")
+}
 
 // Default configures the default target.
 var Default = build.BuildAll
