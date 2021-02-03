d2313a0f1b06607aa40c08e2f6fcc1e9dea90d4b
Marcus Olsson <marcus.olsson@hey.com>
2021-02-03 17:09:39 +0100

    Fix build issues

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './DataSource';
+import { DataSource } from './datasource';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
 import { MyQuery, MyDataSourceOptions } from './types';
